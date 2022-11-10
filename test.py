5c5f966497aefc62e82807194b298cdf723e5e15
onuyonnu <sushioman.OD@gmail.com>
2022-11-09 20:22:03 -0500

    win con set

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-import tkinter as tk
-root = tk.Tk()
-def update_btn_text():
-    btn_text.set("b")
-btn_text = tk.StringVar()
-btn = tk.Button(root, textvariable=btn_text, command=update_btn_text)
-btn_text.set("a")
-btn.pack()
-root.mainloop()
+list = [['1','2','3'], ['4','5','6']]
 
+for line in list:
+    print(line)
+    for spot in range(len(line)):
+        if line[spot] != 1:
+            line[spot] = 1
+print(list)
